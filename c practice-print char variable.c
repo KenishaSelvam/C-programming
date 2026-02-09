aaab25c669bbe4d23af25565847b6a1c8ac0cd95
KenishaSelvam <kenishaselvam@gmail.com>
2026-02-09 16:19:29 +0530

    Add files via upload

---
+++ c practice-print char variable.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    char ch='M';
+    printf("%c",ch);
+
+    return 0;
+}
---
+++ c practice-size of int.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    printf("%d",sizeof(int));
+
+    return 0;
+}
