31821be16f473be07a5546894affffbad05a93e2
KenishaSelvam <kenishaselvam@gmail.com>
2026-02-12 10:23:09 +0530

    Add files via upload

---
+++ c practice-voting eligible.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+
+    int age;
+    age=18;
+    if(age>=18)
+    {
+        printf("eligible to vote");
+    }
+    else
+    {
+       printf("not eligible to vote"); 
+    }
+        return 0;
+}
