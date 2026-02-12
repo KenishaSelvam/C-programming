4cf45614a526371eb827fb2ac6dee0986c03d5c8
KenishaSelvam <kenishaselvam@gmail.com>
2026-02-12 15:09:08 +0530

    Add files via upload

---
+++ c practice-number within range.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int num;
+    scanf("%d",&num);
+    if(num>=10 && num<=20){
+    printf("number is within range");
+    }
+    else{
+    printf("number is not within range");
+    
+    }
+    return 0;
+}
