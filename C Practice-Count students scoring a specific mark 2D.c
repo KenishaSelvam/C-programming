a90f6e5aec9a4c46fea0df95da53a173d04f9bff
KenishaSelvam <kenishaselvam@gmail.com>
2026-03-10 09:34:51 +0530

    Add files via upload

---
+++ C Practice-Count students scoring a specific mark 2D.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,a[100],i,x;
+    int count=0;
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+
+        scanf("%d",&a[i]);
+        scanf("%d",&x);
+        
+        for(i=0;i<n;i++)
+        {
+        if(a[i]==x)
+        count++;
+        }
+    
+    printf("%d",count);
+
+    return 0;
+}
